d37eb082c35b0d4de0558aa115644d6ee7321a39
Antonio <antonio@Antonios-MacBook-Pro-2.local>
2024-02-03 17:38:04 -0500

    Writing notes about Swift types into basic groups like struct class and enums

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,38 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+///*     Properties - Values associated with a type
+///    Initializers - Code that initializes an instance of a type
+///    Instance methods - Functions specific to a type that can be called on an instance of that type
+///    Class or static methods - functions specific to a type that can be called on the type itself
+///
+///    Structures
+///    struct MyStruct {
+///     // properties
+///     // initializers
+///     // methods
+///    }
+///
+///    Enumerations
+///    enums  Myenum {
+///     // properties
+///     // initializers
+///     // methods
+///    }
+///
+///    Classes
+///    class Myclass: Superclass {
+///     // properties
+///     // initializers
+///     // methods
+///    }
+///
+///
+///    Numbers: Int, Float, Double
+///    Boolean: Bool
+///    Text: String, Character
+///    Collections: Array <Element>, Dictionary<Key: Hashable, Value>, Set <Element: Hashable>
+///
+///
+/// */
